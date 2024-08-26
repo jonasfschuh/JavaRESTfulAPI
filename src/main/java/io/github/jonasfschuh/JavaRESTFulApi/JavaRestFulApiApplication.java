b6979f450d6d14aa49fb7553225ff2a8e29df64e
kenzor1979 <jonasschuh@hotmail.com>
2024-08-25 21:14:28 -0300

    initial commit.

---
+++ src/main/java/io/github/jonasfschuh/JavaRESTFulApi/JavaRestFulApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.jonasfschuh.JavaRESTFulApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestFulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRestFulApiApplication.class, args);
+	}
+
+}
