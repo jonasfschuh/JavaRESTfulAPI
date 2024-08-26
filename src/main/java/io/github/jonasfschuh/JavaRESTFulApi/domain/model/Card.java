0ee80b9ea6059e7a11aae9ecf0d5101c23c9f36c
kenzor1979 <jonasschuh@hotmail.com>
2024-08-25 23:25:36 -0300

    precision = 13, scale = 2 adjust

--- src/main/java/io/github/jonasfschuh/JavaRESTFulApi/domain/model/Card.java
+++ src/main/java/io/github/jonasfschuh/JavaRESTFulApi/domain/model/Card.java
@@ -13,7 +13,7 @@ public class Card {
     @Column(unique = true)
     private String number;
 
-    @Column(name = "available_limit", scale = 13, precision = 2)
+    @Column(name = "available_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
