743886a1a51609431bcf6bcf58ec3d4c89dd9b97
kenzor1979 <jonasschuh@hotmail.com>
2024-08-26 02:12:54 -0300

    Cors adjust

--- src/main/java/io/github/jonasfschuh/JavaRESTFulApi/JavaRestFulApiApplication.java
+++ src/main/java/io/github/jonasfschuh/JavaRESTFulApi/JavaRestFulApiApplication.java
@@ -1,8 +1,11 @@
 package io.github.jonasfschuh.JavaRESTFulApi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default server url")})
 @SpringBootApplication
 public class JavaRestFulApiApplication {
 
